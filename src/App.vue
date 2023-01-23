b317234ab2153986dfea043d84366d8a6e9c5e6b
Aurora-Bizzo <aurora.bizzozero98@gmail.com>
2023-01-23 14:02:22 +0100

    clean start

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import axios from 'axios';
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default {
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter
+  }
+}
+</script>
+
+<template lang="">
+  <div>
+    
+  </div>
+</template>
+
+<style lang="">
+  
+</style>
